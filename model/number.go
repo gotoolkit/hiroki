84117c0aa99902a838d794905213be82e6ac8bbb
jack <linjue@wilead.com>
2018-04-24 00:29:01 +0800

    no message

--- model/number.go
+++ model/number.go
@@ -3,12 +3,12 @@ package model
 import "github.com/jinzhu/gorm"
 
 type Number struct {
-	gorm.Model
-	Reds      string `gorm:"type:char(17);not null"`
-	Beginning string `gorm:"type:char(2);not null"`
-	Ending    string `gorm:"type:char(2);not null"`
+    gorm.Model
+    Reds      string `gorm:"type:char(17);not null"`
+    Beginning string `gorm:"type:char(2);not null"`
+    Ending    string `gorm:"type:char(2);not null"`
 }
 
 func (Number) TableName() string {
-	return "number"
+    return "number"
 }
