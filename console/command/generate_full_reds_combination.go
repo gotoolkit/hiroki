2762eb1a396c346f74e43f75673488a18ef566ae
jack <linjue@wilead.com>
2018-04-24 18:08:22 +0800

    [UPDATE]

--- console/command/generate_full_reds_combination.go
+++ console/command/generate_full_reds_combination.go
@@ -4,17 +4,14 @@ import (
     "github.com/urfave/cli"
     "hiroki/database"
     "hiroki/util/math"
-    "hiroki/model"
     "fmt"
-    "strings"
-    "github.com/jinzhu/gorm"
-    "hiroki/util"
     "gopkg.in/cheggaaa/pb.v2"
+    "strings"
+    "hiroki/util"
 )
 
 func GenerateFullRedsCombination(c *cli.Context) error {
-    var tx *gorm.DB
-    db := database.DB
+    db := database.DB.DB()
 
     iterable := make([]int, 33)
     for i := 1; i <= 33; i++ {
@@ -24,42 +21,52 @@ func GenerateFullRedsCombination(c *cli.Context) error {
     // 全组合数据
     numbers := math.Combination(iterable, 6)
 
+    buffer := 10000
     bar := pb.StartNew(len(numbers))
-    i := 0
+    insertStmts := make([]string, 0, buffer)
+    execstring := "INSERT INTO number (reds, beginning, ending, ac, qu_jian, ji_ou, da_xiao, sum, tong_wei, lian_hao, max_lian_hao, kua_ju) VALUES "
     for _, item := range numbers {
-        if i == 0 {
-            tx = db.Begin()
-        }
-
-        entity := &model.Number{}
         reds := [6]string{}
         for i, red := range item {
             reds[i] = fmt.Sprintf("%02d", red)
         }
 
-        entity.Reds = strings.Join(reds[:], ",")
-        entity.Beginning = reds[0]
-        entity.Ending = reds[5]
-        entity.AC = uint8(util.GetAC(item))
-        entity.QuJian = util.GetQuJian(item)
-        entity.JiOu = util.GetJiOu(item)
-        entity.DaXiao = util.GetDaXiao(item)
-        entity.Sum = uint(util.GetSum(item))
-        entity.TongWei = uint8(util.GetTongWei(item))
-        entity.LianHao = uint8(util.GetLianHao(item))
-        entity.MaxLianHao = uint8(util.GetMaxLianHao(item))
-        entity.KuaJu = uint8(util.GetKuaJu(item))
+        stmt := fmt.Sprintf(
+            "(\"%s\", \"%s\", \"%s\", %d, \"%s\", \"%s\", \"%s\", %d, %d, %d, %d, %d)",
+            strings.Join(reds[:], ","),
+            reds[0],
+            reds[5],
+            uint8(util.GetAC(item)),
+            util.GetQuJian(item),
+            util.GetJiOu(item),
+            util.GetDaXiao(item),
+            uint(util.GetSum(item)),
+            uint8(util.GetTongWei(item)),
+            uint8(util.GetLianHao(item)),
+            uint8(util.GetMaxLianHao(item)),
+            uint8(util.GetKuaJu(item)),
+        )
+        insertStmts = append(insertStmts, stmt)
 
-        db.Create(entity)
-        i++
-
-        if i == 2000 {
-            tx.Commit()
-            i = 0
+        if len(insertStmts) == cap(insertStmts) {
+            _, err := db.Exec(execstring + strings.Join(insertStmts, ","))
+            if err != nil {
+                return err
+            }
+            insertStmts = nil
+            insertStmts = make([]string, 0, buffer)
         }
 
         bar.Increment()
     }
+
+    if len(insertStmts) > 0 {
+        _, err := db.Exec(execstring + strings.Join(insertStmts, ","))
+        if err != nil {
+            return err
+        }
+    }
+
     bar.Finish()
     return nil
 }
